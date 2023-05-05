7e67f032e882269292c92136178ce0ce07c87cbf
Corvus <brandonwilliamsjessup@gmail.com>
2023-05-04 23:30:19 -0400

    Fix tiles not being drawn

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -2,7 +2,7 @@
 
 bool TileMap::load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
 {
-    if (_tileset.loadFromFile(tileset))
+    if (!_tileset.loadFromFile(tileset))
     {
         return false;
     }
