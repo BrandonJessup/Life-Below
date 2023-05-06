3ce76d7a20843b8dc8062af0bea3641505c67f2d
Corvus <brandonwilliamsjessup@gmail.com>
2023-05-06 02:16:07 -0400

    Load level from text file

--- src/Tiles.cpp
+++ src/Tiles.cpp
@@ -2,15 +2,16 @@
 
 Tiles::Tiles()
 {
-    const int level[] =
-    {
-        0, 0, 0, 0, 0, 0,
-        0, 1, 1, 1, 1, 0,
-        0, 1, 1, 1, 1, 0,
-        0, 2, 1, 1, 1, 0,
-        0, 1, 1, 1, 2, 0,
-        0, 0, 0, 0, 0, 0
-    };
+    // const int level[] =
+    // {
+    //     0, 0, 0, 0, 0, 0,
+    //     0, 1, 1, 1, 1, 0,
+    //     0, 1, 1, 1, 1, 0,
+    //     0, 2, 1, 1, 1, 0,
+    //     0, 1, 1, 1, 2, 0,
+    //     0, 0, 0, 0, 0, 0
+    // };
 
-    load("assets/tile sheet.png", sf::Vector2u(48, 48), level, 6, 6);
+    // load("assets/tile sheet.png", sf::Vector2u(48, 48), level, 6, 6);
+    loadFromFile("assets/tile sheet.png", sf::Vector2u(48, 48), "level.txt");
 }
