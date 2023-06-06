d036f2df7a3adf07d8158d276e90d0283855e283
Corvus <brandonwilliamsjessup@gmail.com>
2023-06-06 02:03:01 -0400

    Add blank files for user interface class to be used later

---
+++ src/UserInterface.cpp
@@ -0,0 +1,3 @@
+#include "headers/UserInterface.h"
+
+// TODO
---
+++ src/headers/UserInterface.h
@@ -0,0 +1,9 @@
+#ifndef USER_INTERFACE_H
+#define USER_INTERFACE_H
+
+class UserInterface
+{
+    // TODO
+};
+
+#endif // USER_INTERFACE_H
